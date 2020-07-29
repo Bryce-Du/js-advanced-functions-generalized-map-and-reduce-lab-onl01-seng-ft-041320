b073e4411a7f65d27b24099990c083c45648972c
Bryce-Du <bryce.dumas@gmail.com>
2020-07-29 09:23:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(array, func){
+    const returnArray = []
+    for (let i of array){
+        returnArray.push(func(i))
+    }
+    return returnArray
+}
+
+function reduce(array, func, memo){
+    let value
+    if (typeof func(array[0], value) === "number"){value = 0}
+    else {value = true}
+    for(let i of array){
+        value = func(i, value)
+    }
+    if(memo){value += memo}
+    return value
+}
